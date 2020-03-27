1a0c48274e257247b0176a6c669cd8a3b5a4f632
gakanjira <gakanjira@gmail.com>
2020-03-27 15:20:03 +0100

    post infinity scroll

---
+++ Post infinity scroll/script.js
@@ -0,0 +1,73 @@
+const postContainer = document.getElementById('post-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 7;
+let page = 1;
+
+const getPosts = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+
+  const data = await res.json();
+
+  return data;
+}
+
+const showPosts = async () => {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postContainer.appendChild(postEl);
+  })
+}
+
+showPosts();
+
+
+const showLoading = () => {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+const filterPosts = e => {
+  const term = e.target.value;
+  const posts = document.querySelectorAll('.post');
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText
+    const body = post.querySelector('.post-body').innerText
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts)
